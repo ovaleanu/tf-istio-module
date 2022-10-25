0e6fe02062f83fc5278b9d1c2b5ba02c3af276e8
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2022-10-25 14:45:24 +0000

    minor updates

--- main.tf
+++ main.tf
@@ -5,35 +5,27 @@ resource "kubernetes_namespace" "istio_system" {
 }
 
 resource "helm_release" "istio-base" {
-  repository = var.helm_repo_url
-  chart      = "base"
-  name       = "istio-base"
-  namespace  = kubernetes_namespace.istio_system.metadata.0.name
-  version    = var.istio_version
-  timeout          = var.timeout
-  cleanup_on_fail  = var.cleanup_on_fail
-  force_update     = var.force_update
-  depends_on = [kubernetes_namespace.istio_system]
-
-  values = [
-    yamlencode(var.base_settings)
-  ]
+  repository      = var.helm_repo_url
+  chart           = "base"
+  name            = "istio-base"
+  namespace       = kubernetes_namespace.istio_system.metadata.0.name
+  version         = var.istio_version
+  timeout         = var.timeout
+  cleanup_on_fail = var.cleanup_on_fail
+  force_update    = var.force_update
+  depends_on      = [kubernetes_namespace.istio_system]
 }
 
 resource "helm_release" "istiod" {
-  repository = var.helm_repo_url
-  chart      = "istiod"
-  name       = "istiod"
-  namespace  = kubernetes_namespace.istio_system.metadata.0.name
-  version    = var.istio_version
-  timeout          = var.timeout
-  cleanup_on_fail  = var.cleanup_on_fail
-  force_update     = var.force_update
-  depends_on = [helm_release.istio-base]
-
-  values = [
-    yamlencode(var.istiod_settings)
-  ]
+  repository      = var.helm_repo_url
+  chart           = "istiod"
+  name            = "istiod"
+  namespace       = kubernetes_namespace.istio_system.metadata.0.name
+  version         = var.istio_version
+  timeout         = var.timeout
+  cleanup_on_fail = var.cleanup_on_fail
+  force_update    = var.force_update
+  depends_on      = [helm_release.istio-base]
 }
 
 resource "kubernetes_namespace" "istio_ingress" {
@@ -46,13 +38,13 @@ resource "kubernetes_namespace" "istio_ingress" {
 }
 
 resource "helm_release" "istio-ingressgateway" {
-  repository = var.helm_repo_url
-  chart      = "gateway"
-  name       = "istio-ingressgateway"
-  namespace  = kubernetes_namespace.istio_ingress.metadata.0.name
-  version    = var.istio_version
-  timeout          = var.timeout
-  cleanup_on_fail  = var.cleanup_on_fail
-  force_update     = var.force_update
-  depends_on = [kubernetes_namespace.istio_ingress, helm_release.istiod]
+  repository      = var.helm_repo_url
+  chart           = "gateway"
+  name            = "istio-ingressgateway"
+  namespace       = kubernetes_namespace.istio_ingress.metadata.0.name
+  version         = var.istio_version
+  timeout         = var.timeout
+  cleanup_on_fail = var.cleanup_on_fail
+  force_update    = var.force_update
+  depends_on      = [kubernetes_namespace.istio_ingress, helm_release.istiod]
 }
