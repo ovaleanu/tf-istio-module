7df7a6d0106400900fe0911d0425585a4df406a5
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2022-10-25 17:20:40 +0000

    add parameters

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  subnets = join("\\,", var.subnets)
+}
+
 resource "kubernetes_namespace" "istio_system" {
   metadata {
     name = "istio-system"
@@ -14,6 +18,10 @@ resource "helm_release" "istio-base" {
   cleanup_on_fail = var.cleanup_on_fail
   force_update    = var.force_update
   depends_on      = [kubernetes_namespace.istio_system]
+
+  values = [
+    yamlencode(var.istio_base_settings)
+  ]
 }
 
 resource "helm_release" "istiod" {
@@ -26,6 +34,36 @@ resource "helm_release" "istiod" {
   cleanup_on_fail = var.cleanup_on_fail
   force_update    = var.force_update
   depends_on      = [helm_release.istio-base]
+
+  set {
+    name  = "global.meshID"
+    value = var.istio_meshconfig_mesh_name
+  }
+
+  set {
+    name  = "global.network"
+    value = var.istio_meshconfig_network
+  }
+
+  set {
+    name  = "meshConfig.rootNamespace"
+    value = "istio-system"
+  }
+
+  set {
+    name  = "meshConfig.trustDomain"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "meshConfig.accessLogFile"
+    value = "/dev/stdout"
+  }
+
+  values = [
+    yamlencode(var.istiod_settings)
+  ]
+
 }
 
 resource "kubernetes_namespace" "istio_ingress" {
@@ -47,4 +85,45 @@ resource "helm_release" "istio-ingressgateway" {
   cleanup_on_fail = var.cleanup_on_fail
   force_update    = var.force_update
   depends_on      = [kubernetes_namespace.istio_ingress, helm_release.istiod]
+
+  set {
+    name  = "labels.topology\\.istio\\.io/network"
+    value = var.istio_meshconfig_network
+  }
+
+  set {
+    name  = "service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-subnets"
+    value = local.subnets
+  }
+
+  values = [
+    yamlencode(var.elb_load_balancer))
+  ]
+}
+
+data "aws_secretsmanager_secret_version" "ca_private_key" {
+  count     = var.enable_aws_secret_manager_based_certs ? 1 : 0
+  secret_id = var.ca_private_key
+}
+data "aws_secretsmanager_secret_version" "ca_cert_chain" {
+  count     = var.enable_aws_secret_manager_based_certs ? 1 : 0
+  secret_id = var.ca_cert_chain
+}
+data "aws_secretsmanager_secret_version" "ca_cert" {
+  count     = var.enable_aws_secret_manager_based_certs ? 1 : 0
+  secret_id = var.ca_cert
+}
+resource "kubernetes_secret" "istio-ca" {
+  count      = var.enable_aws_secret_manager_based_certs ? 1 : 0
+  depends_on = [helm_release.istio-base]
+  metadata {
+    name      = "cacerts"
+    namespace = "istio-system"
+  }
+  data = {
+    "ca-cert.pem"    = data.aws_secretsmanager_secret_version.ca_cert[count.index].secret_string
+    "cert-chain.pem" = data.aws_secretsmanager_secret_version.ca_cert[count.index].secret_string
+    "ca-key.pem"     = data.aws_secretsmanager_secret_version.ca_private_key[count.index].secret_string
+    "root-cert.pem"  = data.aws_secretsmanager_secret_version.ca_cert_chain[count.index].secret_string
+  }
 }
--- variables.tf
+++ variables.tf
@@ -27,3 +27,98 @@ variable "helm_repo_url" {
   default     = "https://istio-release.storage.googleapis.com/charts"
   description = "Helm repository"
 }
+
+variable "cluster_name" {
+  description = "k8s cluster name, required"
+}
+
+variable "subnets" {
+  description = "provide the subnets used by load balancer for istio gateway"
+  default     = []
+}
+
+variable "istio_aws_elb_gw_enabled" {
+  description = "enable or disable the istio gw install that has an ELB for load balancer, default true"
+  default     = false
+}
+
+variable "elb_load_balancer" {
+  default = {
+    "service" = {
+      "annotations" = {
+        "service.beta.kubernetes.io/aws-load-balancer-internal" = "true"
+      },
+      "ports" = [
+        {
+          "name"       = "https"
+          "port"       = 443
+          "protocol"   = "TCP"
+          "targetPort" = 443
+        },
+        {
+          "name"       = "http2"
+          "port"       = 80
+          "protocol"   = "TCP"
+          "targetPort" = 80
+        },
+        {
+          "name"       = "status-port"
+          "port"       = 15021
+          "protocol"   = "TCP"
+          "targetPort" = 15021
+        },
+        {
+          "name"       = "grpc"
+          "port"       = 50051
+          "protocol"   = "TCP"
+          "targetPort" = 50051
+        },
+        {
+          "name"       = "tls"
+          "port"       = 15443
+          "protocol"   = "TCP"
+          "targetPort" = 15443
+        },
+      ]
+    }
+  }
+}
+
+variable "istio_base_settings" {
+  type        = map(any)
+  default     = {}
+  description = "Additional settings which will be passed to the Helm chart values"
+}
+
+variable "istiod_settings" {
+  type        = map(any)
+  default     = {}
+  description = "Additional settings which will be passed to the Helm chart values"
+}
+
+variable "istio_meshconfig_network" {
+  description = "Istio telementry network name"
+  default     = "network1"
+}
+
+variable "istio_meshconfig_mesh_name" {
+  description = "Istio telementry mesh name"
+  default     = "mesh1"
+}
+
+variable "ca_private_key" {
+  description = "the aws secret arn to use for ca_private_key"
+  default     = ""
+}
+variable "ca_cert_chain" {
+  description = "the aws secret arn to use for the ca_cert_chain"
+  default     = ""
+}
+variable "ca_cert" {
+  description = "the aws secret arn to use for the ca_cert"
+  default     = ""
+}
+variable "enable_aws_secret_manager_based_certs" {
+  description = "If you would like to provide your own mTLS CA certs for istio to use, enable this flag and input AWS secret ARNs required"
+  default     = false
+}
